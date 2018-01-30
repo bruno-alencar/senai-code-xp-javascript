cec7f29389d15b92e64016d077bfec1b59194620
Bruno Alencar <bruno.idealencar@gmail.com>
2018-01-30 21:45:58 -0200

    Refactor to use encapsulation class

--- post-it/js/notes-with-method.js
+++ post-it/js/notes-with-method.js
@@ -1,9 +1,8 @@
 var notes = [
-
-
 ];
 
 var notesList = {
+    section: document.getElementsByClassName("notes")[0],
     internList: [],
     create: function (title, content) {
         var note = {
@@ -13,26 +12,24 @@ var notesList = {
         };
 
         this.internList.push(note);
+        
+        updateSection(this.section);
     },
     delete: function (id) {
         this.internList.splice(id, 1);
+        updateSection(this.section);
     },
     update: function (id) {
         this.internList[id].editing = true;
+        updateSection(this.section);
     },
     save: function (id, newTitle, newContent) {
         this.internList[id].title = newTitle;
         this.internList[id].content = newContent;
         this.internList[id].editing = false;
+        updateSection(this.section);
     }
 }
-
-var note = {
-    id: "",
-    title: "",
-    content: "",
-    editing: true,
-};
 
 var count = 0;
 
@@ -58,10 +55,12 @@ function updateSection(section) {
     //     }
     // });
 
+    var notes = notesList.internList;
+
     for (let i = 0; i < notes.length; i++) {
         if (!notes[i].editing) {
-            contentNotes += '<form id="note-' + i + '" class="note" onclick="updateForm(' + i + ', this.parentElement)">' +
-                '<button class="note__control" type="button" onclick="removeNote(event,' + i + ', this.form.parentElement)">' +
+            contentNotes += '<form id="note-' + i + '" class="note" onclick="updateForm(' + i + ')">' +
+                '<button class="note__control" type="button" onclick="removeNote(event,' + i + ')">' +
                 '<i class="fa fa-times" aria-hidden="true"></i>' +
                 '</button>' +
                 '<h1 class="note__title">' + notes[i].title + '</h1>' +
@@ -71,7 +70,7 @@ function updateSection(section) {
             contentNotes += '<form class="note">' +
                 '<input class="note__title" type="text" name="titulo" placeholder="Título" autofocus value="' + notes[i].title + '" />' +
                 '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + notes[i].content + '</textarea>' +
-                '<button class="note__control" type="button" onclick="updateNote(' + i + ', this.form.titulo, this.form.texto, this.form.parentElement)">Alterar</button>' +
+                '<button class="note__control" type="button" onclick="updateNote(' + i + ', this.form.titulo, this.form.texto)">Alterar</button>' +
                 '</form>';
         }
     }
@@ -79,45 +78,22 @@ function updateSection(section) {
     section.innerHTML = contentNotes;
 }
 
-function updateForm(id, section) {
-    notes[id].editing = true;
-
-    updateSection(section);
+function updateForm(id) {
+    notesList.update(id);
 }
 
-function createNote(title, content, form, section) {
-    var note = {
-        // id: count++,
-        title: title.value,
-        content: content.value,
-        editing: false
-    };
-
-    notes.push(note);
-
-    updateSection(section);
-
+function createNote(title, content, form) {
+    notesList.create(title.value, content.value);
     form.reset();
 }
 
-function updateNote(id, title, content, section) {
-
-    notes[id].title = title.value;
-    notes[id].content = content.value;
-    notes[id].editing = false;
-
-    updateSection(section);
+function updateNote(id, newTitle, newContent) {
+    notesList.save(id, newTitle.value, newContent.value);
 }
 
-function removeNote(event, id, section) {
-
+function removeNote(event, id) {
     event.stopPropagation();
-
-    // index = notes.map(function (item) {
-    //     return item.Id
-    // }).indexOf(id);
-
-    notes.splice(id, 1);
-
-    updateSection(section);
+    
+    document.getElementById("note-1");
+    notesList.delete(id.value);
 }
