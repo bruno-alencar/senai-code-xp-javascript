6c09d72a3945f41f06608374f6e45d4402a32374
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-01 18:50:17 -0200

    Refactor to add arrow function with es6

--- post-it/js/notes-with-es6.js
+++ post-it/js/notes-with-es6.js
@@ -1,6 +1,4 @@
-var notes = [];
-
-var notesList = {
+const notesList = {
     section: document.getElementsByClassName("notes")[0],
     internList: [],
     create: (title, content) => {
@@ -13,7 +11,7 @@ var notesList = {
         this.internList.push(note);
         updateSection(this.section);
     },
-    delete: id => {  
+    delete: id => {
         this.internList.splice(id, 1);
         updateSection(this.section);
     },
@@ -28,36 +26,15 @@ var notesList = {
         updateSection(this.section);
     },
     get: id => this.internList[id],
-    totalCount: _ => this.internList.length
+    totalCount: () => this.internList.length
 }
 
-var count = 0;
-
-function updateSection(section) {
-    var contentNotes = "";
-
-    // notes.forEach(element => {
-
-    //     if (!element.editing) {
-    //         contentNotes +=<form id="note-' + element.id + '" class="note" onclick="updateForm(' + element.id + ', this.parentElement)">' +
-    //             '<button class="note__control" type="button" onclick="removeNote(event,' + element.id + ', this.form.parentElement)">' +
-    //             '<i class="fa fa-times" aria-hidden="true"></i>' +
-    //             '</button>' +
-    //             '<h1 class="note__title">' + element.title + '</h1>' +
-    //             '<p class="note__body">' + element.content + '</p>' +
-    //             '</form>';
-    //     } else {
-    //         contentNotes += '<form class="note">' +
-    //             '<input class="note__title" type="text" name="titulo" placeholder="Título" autofocus value="' + element.title + '" />' +
-    //             '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + element.content + '</textarea>' +
-    //             '<button class="note__control" type="button" onclick="updateNote(' + element.id + ', this.form.titulo, this.form.texto, this.form.parentElement)">Alterar</button>' +
-    //             '</form>';
-    //     }
-    // });
+const updateSection = section => {
+    let contentNotes = "";
 
     for (let i = 0; i < notesList.totalCount(); i++) {
 
-        var note = notesList.get(i);
+        let note = notesList.get(i);
 
         if (!note.editing) {
             contentNotes += `<form id="note-${i}" class="note" onclick="updateForm( ${i})">
@@ -68,7 +45,7 @@ function updateSection(section) {
                 <p class="note__body"> ${note.content}</p>
                 </form>`;
         } else {
-            contentNotes +=`<form class="note">
+            contentNotes += `<form class="note">
                <input class="note__title" type="text" name="titulo" placeholder="Título" autofocus value=" ${note.title}"/>
                <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota..."> ${note.content} </textarea>
                <button class="note__control" type="button" onclick="updateNote(${i}, this.form.titulo, this.form.texto)">Alterar</button>
@@ -79,20 +56,16 @@ function updateSection(section) {
     section.innerHTML = contentNotes;
 }
 
-function updateForm(id) {
-    notesList.update(id);
-}
+const updateForm = id => notesList.update(id);
 
-function createNote(title, content, form) {
+const createNote = (title, content, form) => {
     notesList.create(title.value, content.value);
     form.reset();
 }
 
-function updateNote(id, newTitle, newContent) {
-    notesList.save(id, newTitle.value, newContent.value);
-}
+const updateNote = (id, newTitle, newContent) => notesList.save(id, newTitle.value, newContent.value);
 
-function removeNote(event, id) {
+const removeNote = (event, id) => {
     event.stopPropagation();
 
     var millisecondsToWait = 500;
