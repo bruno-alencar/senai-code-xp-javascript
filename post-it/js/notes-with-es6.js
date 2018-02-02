8d94d44a0e4c6c6a65725b15054a33bfcfbbfdfb
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-02 19:47:06 -0200

    Add validation on get and set note class methods

--- post-it/js/notes-with-es6.js
+++ post-it/js/notes-with-es6.js
@@ -1,8 +1,33 @@
 class Note {
     constructor(title, content){
-        this.title = title;
-        this.content = content;
-        this.status = false;
+        this._title = title;
+        this._content = content;
+        this._status = false;
+    }
+
+    get title(){
+        return this._title;
+    }
+    
+    set title(title){
+
+        (title !== null && title.length > 5) ? _title = title : alert("Title is not valid!");
+    }
+
+    get content(){
+        return this._content;
+    }
+
+    set content(content){
+        (content !== null && content.length > 5) ? _content = content : alert("Content is not valid!");
+    }
+    
+    get status(){
+        return this._status;
+    }
+
+    set content(status){
+        this._status = status;
     }
 }
 
