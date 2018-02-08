e963de9fd5db10b93ddc5272df12b89ae1ceee45
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-08 18:23:45 -0200

    Refactor to create note using new functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ListNotes from './listNotes.js';
+// import FormNotes from './components/formNotes.js';
 import FormInput from './components/formInput.js';
+import FormTextArea from './components/formTextArea.js';
+import FormButton from './components/formButton.js';
+import Form from './components/form.js';
 
 const section = document.getElementsByClassName('notes')[0];
 
@@ -12,53 +16,97 @@ const notesList = new ListNotes(observerList);
 const updateSection = section => {
     let contentNotes = "";
 
+    while (section  .firstChild) {
+        section.removeChild(section.firstChild);
+    }
+
     for (let i = 0; i < notesList.totalCount(); i++) {
 
         let note = notesList.get(i);
+        // const form = new FormNotes({note: note, position: i, updateNote, removeNote, updateForm});
+        let form;
+        // section.appendChild(form);
+        if (note.editing) {
 
-        if (!note.editing) {
-
-
-            contentNotes += `<form id="note-${i}" class="note" onclick="updateForm( ${i})">
-                <button class="note__control" type="button" onclick="removeNote(event, ${i})">
-                <i class="fa fa-times" aria-hidden="true"></i>
-                </button>
-                <h1 class="note__title"> ${note.title}</h1>
-                <p class="note__body"> ${note.content}</p>
-                </form>`;
+                let inputTitle = new FormInput({
+                    class: 'note__title',
+                    type: 'text',
+                    name: 'titulo',
+                    placeholder: 'Título',
+                    value:  note.title,
+                    readonly: !note.editing
+                });
+                
+                let textArea = new FormTextArea({
+                    class: 'note__body',
+                    name: 'texto',
+                    rows: 5,
+                    placeholder: 'Criar uma nota...',
+                    value:  note.content
+                });
+                
+                let buttonItem = new FormButton({
+                    class: 'note__control',
+                    type: 'button',
+                    children: 'Alterar',
+                    event: () => updateNote(i, inputTitle, textArea)
+                });
+                
+                form = new Form({
+                    class: 'note',
+                    event: () => {},
+                    children: [inputTitle, textArea, buttonItem]
+                });
+                
+                // contentNotes += `<form class="note">
+                //    <input class="note__title" type="text" name="titulo" placeholder="Título" autofocus value=" ${note.title}"/>
+                //    <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota..."> ${note.content} </textarea>
+                //    <button class="note__control" type="button" onclick="updateNote(${i}, this.form.titulo, this.form.texto)">Alterar</button>
+                //    </form>`;
         } else {
-
-            let noteForm = document.createElement('form');
-            noteForm.setAttribute('class', 'notes');
-
-            let inputTitle = new FormInput();
-
-            let textArea = document.createElement('textarea');
-            textArea.setAttribute('class', 'note__body');
-            textArea.setAttribute('name', 'texto');
-            textArea.setAttribute('rows', '5');
-            textArea.setAttribute('placeholder', 'Criar uma nota...');
-            // textArea.value = note.content;
-            textArea.innerHTML = note.content;
-
-            let buttonItem = document.createElement('button');
-            buttonItem.setAttribute('class', 'note__control');
-            buttonItem.setAttribute('type', 'button');
-            buttonItem.addEventListener('click', (e) => {
-                updateNote(i, inputTitle, textArea);
-                // updateNote(i, event.target.title, event.target.content);
+            
+            let buttonItem = new FormButton({
+                class: 'note__control',
+                type: 'button',
+                children: '<i class="fa fa-times" aria-hidden="true"></i>',
+                event: e => removeNote(e, i)
+            });
+            
+            let inputTitle = new FormInput({
+                class: 'note__title',
+                type: 'text',
+                name: 'titulo',
+                placeholder: 'Título',
+                value:  note.title,
+                readonly: !note.editing
+            });
+            
+            let textArea = new FormTextArea({
+                class: 'note__body',
+                name: 'texto',
+                rows: 5,
+                placeholder: 'Criar uma nota...',
+                value:  note.content
+            });
+            
+            form = new Form({
+                id: `note-${i}`,
+                class: 'note',
+                event: () => updateForm(i),
+                children: [buttonItem, inputTitle, textArea]
             });
 
-
-            contentNotes += `<form class="note">
-               <input class="note__title" type="text" name="titulo" placeholder="Título" autofocus value=" ${note.title}"/>
-               <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota..."> ${note.content} </textarea>
-               <button class="note__control" type="button" onclick="updateNote(${i}, this.form.titulo, this.form.texto)">Alterar</button>
-               </form>`;
+            // contentNotes += `<form id="note-${i}" class="note" onclick="updateForm( ${i})">
+            //     <button class="note__control" type="button" onclick="removeNote(event, ${i})">
+            //     <i class="fa fa-times" aria-hidden="true"></i>
+            //     </button>
+            //     <h1 class="note__title"> ${note.title}</h1>
+            //     <p class="note__body"> ${note.content}</p>
+            //     </form>`;
         }
-    }
 
-    section.innerHTML = contentNotes;
+        section.appendChild(form);
+    }
 }
 
 window.updateForm = id => notesList.update(id);
@@ -79,7 +127,7 @@ window.removeNote = (event, id) => {
         item.classList.add('animation-test');
 
         setTimeout(function () {
-            notesList.splice(id.value);
+            notesList.splice(id);
         }, 300);
 
     }, millisecondsToWait);
