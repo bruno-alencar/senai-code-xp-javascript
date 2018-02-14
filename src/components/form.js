91c45f02c8ffa24d47a77b328ce66d326a354af4
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-14 20:09:52 -0200

    Refactor on form to use react syntax

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,22 @@
-function Form(props){
-    let form = document.createElement('form');
+import React from 'react'
 
-    form.setAttribute('id', props.id);
-    form.setAttribute('class', props.class);
-    for (let i = 0; i < props.children.length; i++) {
-        form.appendChild(props.children[i]);
-    }
+export default (props, children) => React.createElement('form', props, children);
+
+// function Form(props){
+//     let form = document.createElement('form');
+
+//     form.setAttribute('id', props.id);
+//     form.setAttribute('class', props.className);
+//     for (let i = 0; i < props.children.length; i++) {
+//         form.appendChild(props.children[i]);
+//     }
     
-    form.addEventListener('click', props.event);
-    return form;
-}
-
-// const propsForm = {
-//     class: 'notes'
+//     form.addEventListener('click', props.event);
+//     return form;
 // }
 
-export default Form;
+// // const propsForm = {
+// //     class: 'notes'
+// // }
+
+// export default Form;
