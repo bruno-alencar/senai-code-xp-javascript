066ab609de6e1cb5401c2316200478e9a18ce9a2
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-15 22:02:55 -0200

    Refactor on index js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import ListNotes from './listNotes.js';
 import FormNotes from './components/formNotes.js';
 import FormInput from './components/formInput.js';
 import FormTextArea from './components/formTextArea.js';
 import FormButton from './components/formButton.js';
 import Form from './components/form.js';
+import SectionNote from './components/notesSection.js';
 
 const section = document.getElementsByClassName('notes')[0];
 
@@ -15,14 +17,18 @@ const notesList = new ListNotes(observerList);
 
 const updateSection = section => {
     // Clean
-    section.innerHTML = '';
+    // section.innerHTML = '';
 
-    for (let i = 0; i < notesList.totalCount(); i++) {
 
-        let note = notesList.get(i);
-        const form = new FormNotes({note: note, position: i, updateNote, removeNote, updateForm});
-        section.appendChild(form);
-    }
+    let props = {notesList, updateNote, removeNote, updateForm};
+    return React.createElement(SectionNote, props);
+
+    // for (let i = 0; i < notesList.totalCount(); i++) {
+
+    //     let note = notesList.get(i);
+    //     const form = new FormNotes({note: note, position: i, updateNote, removeNote, updateForm});
+    //     section.appendChild(form);
+    // }
 }
 
 window.updateForm = id => notesList.update(id);
