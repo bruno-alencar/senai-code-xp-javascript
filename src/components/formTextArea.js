bf6e642b3e10abb4ae5981d55590a31b2558155d
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-21 13:21:29 -0300

    Refactor formTextArea to use jsx syntax

--- src/components/formTextArea.js
+++ src/components/formTextArea.js
@@ -1,26 +1,3 @@
 import React from 'react'
 
-export default props => React.createElement('textarea', props);
-
-// function FormTextArea(props){
-//     let textArea = document.createElement('textarea');
-
-//     textArea.setAttribute('class', props.className);
-//     textArea.setAttribute('name', props.name);
-//     textArea.setAttribute('rows', props.rows);
-//     textArea.setAttribute('placeholder', props.placeholder);
-//     // textArea.value = note.content;
-//     textArea.innerHTML = props.children;
-
-//     return textArea;
-// }
-
-// // const propsTextArea = {
-// //     class: 'note__body',
-// //     name: 'texto',
-// //     rows: 5,
-// //     placeholder: 'Criar uma nota...',
-// //     value:  note.content
-// // }
-
-// export default FormTextArea;
+export default props => <textarea {...props} />
