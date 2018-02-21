debf56c05ef3659ff9504355f5a57eccb68aa845
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-21 13:21:00 -0300

    Refactor formButton to use jsx syntax

--- src/components/formButton.js
+++ src/components/formButton.js
@@ -1,22 +1,3 @@
 import React from 'react'
 
-export default (props, children) => React.createElement('button', props, children);
-
-
-// function FormButton(props, children){
-//     return React.createElement('button', props, children);
-// }
-
-// function FormButton(props) {
-//     let button = document.createElement('button');
-
-//     button.setAttribute('class', props.class);
-//     button.setAttribute('type', props.type);
-//     button.addEventListener('click', props.event);
-
-//     button.innerHTML = props.children;
-
-//     return button;
-// }
-
-// export default FormButton;
+export default ({children, ...props}) => <button {...props}>{children}</button>
