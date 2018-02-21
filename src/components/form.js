696d971f1b4a779d6f0c7ecb84eecf878b6e559a
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-21 13:20:50 -0300

    Refactor form to use jsx syntax

--- src/components/form.js
+++ src/components/form.js
@@ -1,22 +1,3 @@
 import React from 'react'
 
-export default (props, children) => React.createElement('form', props, children);
-
-// function Form(props){
-//     let form = document.createElement('form');
-
-//     form.setAttribute('id', props.id);
-//     form.setAttribute('class', props.className);
-//     for (let i = 0; i < props.children.length; i++) {
-//         form.appendChild(props.children[i]);
-//     }
-    
-//     form.addEventListener('click', props.event);
-//     return form;
-// }
-
-// // const propsForm = {
-// //     class: 'notes'
-// // }
-
-// export default Form;
+export default ({children, ...props}) => <form {...props}>{children}</form>
