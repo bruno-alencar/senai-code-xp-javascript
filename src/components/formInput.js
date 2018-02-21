a59ef646025a8655fcdd8c1115bef910fbc103e4
Bruno Alencar <bruno.idealencar@gmail.com>
2018-02-21 13:21:10 -0300

    Refactor formInput to use jsx syntax

--- src/components/formInput.js
+++ src/components/formInput.js
@@ -1,29 +1,3 @@
 import React from 'react'
 
-export default props => React.createElement('input', props);
-
-
-// function FormInput(props) {
-//     let input = document.createElement('input');
-
-//     input.setAttribute('class', props.className);
-//     input.setAttribute('type', props.type);
-//     input.setAttribute('name', props.name);
-//     input.setAttribute('placeholder', props.placeholder);
-//     input.setAttribute('value', props.value);
-
-//     if(props.readonly) 
-//         input.setAttribute('readonly', true);
-
-//     return input;
-// }
-
-// const propsInput = {
-//     class: 'note__title',
-//     type: 'text',
-//     name: 'titulo',
-//     placeholder: 'Título',
-//     value:  note.title
-// }
-
-// export default FormInput;
+export default props => <input {...props} />
